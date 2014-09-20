5f1feac1ea5c12c9b2ab0a39bb8889cac43f529d
Dan Stocker <dan@kwaia.com>
2014-09-20 18:56:08 +0100

    adds Location.js to Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,6 +8,7 @@ module.exports = function (grunt) {
             'js/utils/Deferred.js',
             'js/utils/Promise.js',
             'js/utils/PromiseCollection.js',
+            'js/utils/Location.js',
             'js/service/serviceEventSpace.js',
             'js/service/Endpoint.js',
             'js/service/Request.js',
